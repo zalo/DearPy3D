79311f82418b251496591c2f106b4d6e062d97fc
Jonathan Hoffstadt <jonathanhoffstadt@yahoo.com>
2021-03-25 20:41:49 -0500

    Pre_CC: removed editor layout work

--- Marvel/main.cpp
+++ Marvel/main.cpp
@@ -36,20 +36,15 @@ int main()
     mvPointLight light(graphics, {10.0f, 5.0f, 0.0f});
 
     // create camera
-    mvCamera camera1(graphics, {-13.5f, 6.0f, 3.5f}, 0.0f, PI / 2.0f);
-    mvCamera camera2(graphics, { 0.0f, 5.0f, -5.0f }, 0.0f, 0.0f, 1.0f, 1.0f);
+    mvCamera camera(graphics, {-13.5f, 6.0f, 3.5f}, 0.0f, PI / 2.0f, 1850.0f, 900.0f);
+    //mvCamera camera(graphics, { 0.0f, 5.0f, -5.0f }, 0.0f, 0.0f, 1.0f, 1.0f);
 
     // create model
-    mvModel model1(graphics, "../../Resources/Models/Sponza/sponza.obj", 1.0f/20.0f);
-    mvModel model2(graphics, "../../Resources/Models/gobber/GoblinX.obj", 1.0f);
-    model2.setRootTransform(glm::translate(glm::vec3{ 0.0f, 5.0f, 0.0f }));
+    mvModel model(graphics, "../../Resources/Models/Sponza/sponza.obj", 1.0f/20.0f);
+    //mvModel model(graphics, "../../Resources/Models/gobber/GoblinX.obj", 1.0f);
 
-    model1.linkTechniques(graph);
+    model.linkTechniques(graph);
     light.linkTechniques(graph);
-    model2.linkTechniques(graph);
-
-    mvRenderTarget target1(graphics, 1500, 835);
-    mvRenderTarget target2(graphics, 300, 300);
 
     // timer
     Marvel::mvTimer timer;
@@ -63,75 +58,28 @@ int main()
 
         const auto dt = timer.mark() * 1.0f;
 
-        HandleEvents(window, dt, camera1);
+        HandleEvents(window, dt, camera);
 
         imManager.beginFrame();
 
-        // first render
-        target1.bindAsBuffer(graphics);
-        target1.clear(graphics);
- 
-        camera1.bind(graphics);
-        light.bind(graphics, camera1.getMatrix());
-        
-        model1.submit(graph);
-        model2.submit(graph);
-        light.submit(graph);
-
-        graph.execute(graphics);
-        graph.reset();
-
-        // second render
-        target2.bindAsBuffer(graphics);
-        target2.clear(graphics);
-
-        camera2.bind(graphics);
-        light.bind(graphics, camera2.getMatrix());
-
-        model1.submit(graph);
-        model2.submit(graph);
-        light.submit(graph);
-
-        graph.execute(graphics);
-        graph.reset();
-
-        // main frame buffer
         graphics.getTarget()->bindAsBuffer(graphics);
         graphics.getTarget()->clear(graphics);
+ 
+        camera.bind(graphics);
+        light.bind(graphics, camera.getMatrix());
+        
+        model.submit(graph);
+        light.submit(graph);
 
-        ImGui::PushStyleVar(ImGuiStyleVar_WindowBorderSize, 0.0f);
-        ImGui::SetNextWindowPos(ImVec2(0.0f, 0.0f));
-        ImGui::SetNextWindowSize(ImVec2(1850.0f, 900.0f));
-        if (ImGui::Begin("Main", nullptr, ImGuiWindowFlags_NoResize | ImGuiWindowFlags_NoCollapse | ImGuiWindowFlags_NoTitleBar))
-        {
-            ImGuiID dockspace_id = ImGui::GetID("Main");
-            ImGui::DockSpace(dockspace_id, ImVec2(0.0f, 0.0f), ImGuiDockNodeFlags_None);
-        }
-        ImGui::End();
-        ImGui::PopStyleVar();
+        light.submit(graph);
+
+        graph.execute(graphics);
+        graph.reset();
 
         light.show_imgui_windows("Light 1");
 
-        ImGui::PushStyleVar(ImGuiStyleVar_WindowPadding, { 0.0f, 0.0f });
-        ImGui::SetNextWindowSize(ImVec2(1500.0f, 855.0f));
-        if (ImGui::Begin("Render1", nullptr))
-        {
-            if (target1.getTarget())
-                ImGui::Image(target1.getShaderResource(), ImVec2(1500, 835));
-        }
-        ImGui::End();
-
-        ImGui::SetNextWindowSize(ImVec2(300, 320));
-        if (ImGui::Begin("Render2", nullptr))
-        {
-            if (target2.getTarget())
-                ImGui::Image(target2.getShaderResource(), ImVec2(300, 300));
-        }
-        ImGui::End();
-        ImGui::PopStyleVar();
-
         ImGuiIO& io = ImGui::GetIO();
-        ImGui::GetForegroundDrawList()->AddText(ImVec2(375, 45),
+        ImGui::GetForegroundDrawList()->AddText(ImVec2(45, 45),
             ImColor(0.0f, 1.0f, 0.0f), std::string(std::to_string(io.Framerate) + " FPS").c_str());
         imManager.endFrame();
 
