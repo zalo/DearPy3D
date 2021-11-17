9a3a00080607500bd5f2fa30d4e15e29d35b859a
Jonathan Hoffstadt <jonathanhoffstadt@yahoo.com>
2021-11-17 14:26:10 -0600

    testing builds

---
+++ Scripts/install_vulkan.ps1
@@ -0,0 +1,2 @@
+Invoke-WebRequest -Uri "https://sdk.lunarg.com/sdk/download/1.2.189.2/windows/VulkanSDK-1.2.189.2-Installer.exe" -OutFile VulkanSDK-1.2.189.2-Installer.exe
+./VulkanSDK-1.2.189.2-Installer.exe
